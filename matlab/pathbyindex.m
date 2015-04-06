7ebd90cec4bf106541e9478289bf3bd5a82a7f59
Steven Buczkowski <steven.buczkowski@gmail.com>
2015-04-06 13:32:27 -0400

    Added file2cell support

--- matlab/pathbyindex.m
+++ matlab/pathbyindex.m
@@ -1,16 +1,9 @@
 function outstr = pathbyindex(infile, index)
-% PATHBYINDEX
+% PATHBYINDEX read in list of 
 %
 % 
 
-% open file infile (which contains a list of file paths for files
-% to be acted upon)
-fh = fopen(infile);
-
-% read list paths into a cell array
-flines = textscan(fh, '%s');
-
-fclose(fh);
+flines = file2cell(infile);
 
 % index into cell array and output requested line
 outstr = flines{1}{index}
